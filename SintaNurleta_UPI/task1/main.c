0727122ba0047ef61f9147930985707f581a13a7
Sinta Nurleta <75920630+Sinta-Nurleta@users.noreply.github.com>
2021-10-04 15:54:14 +0700

    First Submission by Sinta Nurleta
    Task1

---
+++ SintaNurleta_UPI/task1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Nama                : Sinta Nurleta\n");
+    printf("Jurusan             : Pendidikan Multimedia\n");
+    printf("Alasan Masuk GDSC   : 1. bisa ikut serta aktif dalam segala kegiatan yang diadakan\n");
+    printf("\t\t      2. bisa lebih paham akan dunia IT \n");
+    printf("\t\t      3. menambah pengalaman yang menunjang karir untuk terjun di dunia tekologi\n");
+    printf("\t\t      4. bisa lebih berdampak terhadap perubahan yang akan terjadi kedepannya\n");
+    printf("\t\t      5. menjadi pribadi yang paham bagaimana beradaptasi ditengah perkembangan teknologi yang semakin maju\n");
+    printf("\t\t      6. bisa memanfaatkan ilmu serta pengalaman untuk memecahkan masalah yang ada");
+    return 0;
+}
